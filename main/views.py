d1f417e1c385f09843bb48b79aba2c09c902178a
Nanozeka <Nanozeka@gmail.com>
2025-01-04 16:43:50 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context: dict[str, str] = {
+        'title': 'Home',\
+        'content': 'Главная страница сайта - Home',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': True
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
+
