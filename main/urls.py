dfa2c8bf2f46c9a60e55ffe7c7000b010f6cd6d8
Nanozeka <Nanozeka@gmail.com>
2025-01-06 19:50:31 +0300

    Готово приложение main и страницы index и about

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+app_name = 'main'
+from main import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+]
--- main/views.py
+++ main/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 
 def index(request):
     context: dict[str, str] = {
-        'title': 'Home',\
-        'content': 'Главная страница сайта - Home',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': True
+        'title': 'Home - Главная',\
+        'content': 'Магазин мебели Home',
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict[str, str] = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': "Текст об офигенном магазе"
+    }
+    return render(request, 'main/about.html', context)
 
