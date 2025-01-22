480d64404d3224aaf4d0e8ccebabfbe464324edb
Nanozeka <Nanozeka@gmail.com>
2025-01-22 19:50:38 +0300

    Пагинация вариант 2 c get параметрами

--- goods/urls.py
+++ goods/urls.py
@@ -13,6 +13,5 @@ from goods import views
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
